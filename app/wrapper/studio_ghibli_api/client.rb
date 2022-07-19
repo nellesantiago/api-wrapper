fd1431ee056c85cd146854e6f2fbe2c1f601f27d
Junelle Santiago <junellesantiago@gmail.com>
2022-07-19 20:32:07 +0800

    create private method send_request

--- app/wrapper/studio_ghibli_api/client.rb
+++ app/wrapper/studio_ghibli_api/client.rb
@@ -1,5 +1,13 @@
 module StudioGhibliApi
     class Client
         BASE_URL = "https://ghibliapi.herokuapp.com".freeze
+
+        private
+
+        def send_request(http_method, path_to_resource, params={}, headers={})
+            connection = Faraday.new(url: BASE_URL, params: params, headers: headers)
+            response = connection.public_send(http_method, path_to_resource)
+            JSON.parse(response.body)
+        end
     end
 end
