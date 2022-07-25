4a0bfaaebe0ad3d36bf16542266e0c97283be95c
Junelle Santiago <junellesantiago@gmail.com>
2022-07-25 18:26:19 +0800

    refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
 
-  resources :films, only: %i[index show]
-  resources :characters, only: %i[index show]
-  resources :locations, only: %i[index show]
-  resources :species, only: %i[index show]
+  resources :films, only: %i[index]
+  resources :characters, only: %i[index]
+  resources :locations, only: %i[index]
+  resources :species, only: %i[index]
 end
